a41a02589ff832a5327391f1daf2337e16572b5a
Apisak Potisanmetee <iapisak1981@gmail.com>
2019-11-20 15:38:59 -0800

    Add Login component (#4)

--- src/components/Navbars/Navbars.js
+++ src/components/Navbars/Navbars.js
@@ -3,6 +3,7 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Navbars = () => {
         return (
+          <>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="container">
               <Link className="navbar-brand" to="#">WAYFARER</Link>
@@ -16,18 +17,40 @@ const Navbars = () => {
                     <NavLink className="nav-link" to="/">Home</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="#">Sign In</NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="#">Sign Up</NavLink>
-                  </li>
-                  <li className="nav-item">
                     <NavLink className="nav-link" to="#">Profile</NavLink>
+                  </li>
+                  <li className="nav-item" data-toggle="modal" data-target="#exampleModalCenter" >Sign in</li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
                   </li>
                 </ul>
               </div>
             </div>
           </nav>
+
+
+
+          <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  .....
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </>
+
         )
 }
 
