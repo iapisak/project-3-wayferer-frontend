319dbda8ca325c84af7620118e93b4a45e9cd82e
adilkhan <adilkhan@ucsb.edu>
2019-11-21 11:53:32 -0800

    Commit: added uploadCare functionality for uploading picture.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import { Widget } from "@uploadcare/react-widget";
 // import UserInfo from './UserInfo/UserInfo';
 import Postlist from './Postlist/Postlist';
 import './Profile.css';
@@ -10,6 +10,7 @@ class Profile extends Component {
     user: {
         name: 'test user',
         currentCity: 'test city',
+        profilePhoto: ''
     },
   };
 
@@ -21,11 +22,28 @@ class Profile extends Component {
     ).then((res) => {
       console.log(res);
       this.setState({
-        user: res.data.data._doc,
+        user: res.data.data,
       });
     }).catch((err) => console.log(err));
   }
-
+  pictureUploaded =(event)=>{
+    console.log(event.originalUrl)
+    this.setState({
+      user:{profilePhoto: event.originalUrl}
+    })
+    console.log(this.state.user.profilePhoto)
+  }
+  fileUploadHandler=(event)=>{
+    console.log("button working")
+    const photoLink = this.state.user.profilePhoto
+    event.preventDefault()
+    const userId = localStorage.getItem('uid');
+    axios.put(`${process.env.REACT_APP_API_URL}/users/update/${userId}`,{profilePhoto:photoLink}).then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   render() {
     const { user } = this.state;
     return (
@@ -34,6 +52,8 @@ class Profile extends Component {
           <img src={user.profilePhoto} alt={user.name} />
           <h2>{user.name}</h2>
           <h3 className="profile-user-city">{user.currentCity}</h3>
+          <Widget publicKey="38e56a09985595f94b66" onChange={this.pictureUploaded} type="file"/>
+          <button onClick={this.fileUploadHandler}>Submit</button>
         </div>
         <div className="profile-postlist">
           <Postlist />
