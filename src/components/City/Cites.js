adece9ed4ef1a53d159e809b1caa12c2fbb11615
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-21 15:52:06 -0800

    cities route

---
+++ src/components/City/Cites.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Cities extends Component {
+    state = {
+
+    }
+    render(){
+        return(
+            <h1>Cities</h1>
+        )
+    }
+}
+
+export default Cities
