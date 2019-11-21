106528c091149f097b3d454de8a0ebca9235d944
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-21 11:31:52 -0800

    Add a post detail page with route

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from '../components/Home/Home';
 import Profile from '../components/Profile/Profile';
+import PostDetail from '../components/PostDetail/PostDetail';
 
 export default ({ currentUser }) => (
   <Switch>
@@ -10,5 +11,6 @@ export default ({ currentUser }) => (
         <Profile currentUser={currentUser}/>
       )}
       />
+      <Route path="/posts/:post_id" component={PostDetail} />
   </Switch>
 );
