92dd45ef19189f578829a47cc3e1ba7f9b283069
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-20 16:27:48 -0800

    Refactor App.js to set the user and logout

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React from 'react';
 import Navbars from './components/Navbars/Navbars'
 import Routes from './config/Routes'
 
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HIIIIII</h1>
-      <Navbars />
-      <Routes />
-    </div>
-  );
+class App extends Component {
+  state = {
+    currentUser: localStorage.getItem('uid'),
+  };
+
+  setCurrentUser = (userId) => {
+    this.setState({ currentUser: userId });
+    localStorage.setItem('uid', userId);
+  };
+
+  logout = () => {
+    localStorage.removeItem('uid');
+    axios.post(
+      `${process.env.REACT_APP_BASE_API}/auth/logout`,
+      { withCredentials: true }
+    ).then(res => {
+        this.setState({ currentUser: null });
+        this.props.history.push('/login');
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Wayfarer</h1>
+        <Navbars />
+        <Routes
+          currentUser={this.state.currentUser}
+          setCurrentUser={this.setCurrentUser}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
