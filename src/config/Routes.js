cfdf153a0d8c5a9352616da871f59771e0e24312
adilkhan <adilkhan@ucsb.edu>
2019-11-22 09:46:35 -0800

    Commit : Worked on the city frontend

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -4,11 +4,13 @@ import Home from '../components/Home/Home';
 import Profile from '../components/Profile/Profile';
 import PostDetail from '../components/PostDetail/PostDetail';
 import Cities from '../components/City/Cites'
+import CreatePost from '../components/City/CreatePost'
 
 export default ({ currentUser }) => (
   <Switch>
       <Route exact path="/" component={Home} />
       <Route path='/cities' component={Cities}/>
+      <Route exact path='/city/create' component={CreatePost} />
       <Route path="/profile" render={() => (
         <Profile currentUser={currentUser}/>
       )}
