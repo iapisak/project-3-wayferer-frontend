7fa9a928a9bd27d7f884d675b7fca48771971e8e
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-22 14:31:37 -0800

    frontend fixes

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -5,6 +5,7 @@ const City = (props) => {
     return(
         <div onClick = {e => {props.displayPosts(props.city.slug) }}className="city-card">
             <div className="city-icon" style ={{ backgroundImage:`url(${props.city.photo})`}}/>
+            <button></button>
             <h3>{props.city.name}</h3>
         </div>
     )
