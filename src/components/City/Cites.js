d56be51036255a5811e1df0d72bb4d5e3712da13
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-21 16:04:50 -0800

    cities folder

--- src/components/City/Cites.js
+++ src/components/City/Cites.js
@@ -3,7 +3,15 @@ import axios from 'axios';
 
 class Cities extends Component {
     state = {
+        ajaxLoaded:false,
+        cities:[],
+        cityPosts:[]
+    }
 
+    componentDidMount(){
+        axios.get(`${process.env.REACT_APP_API_URL}/cities`).then(cities=>{
+            console.log(cities)
+        })
     }
     render(){
         return(
