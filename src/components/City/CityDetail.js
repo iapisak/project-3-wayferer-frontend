f92179e6cde9ef542ac878983b31b20032b65499
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-24 10:39:57 -0800

    Add some light styling to navbar and city page

--- src/components/City/CityDetail.js
+++ src/components/City/CityDetail.js
@@ -6,7 +6,7 @@ const {city,posts} = props
   return(
   <div className="city-detail">
     <div className ="city-top-banner">
-      <img src={city.photo} className="city-photo"/>
+      <img src={city.photo} className="city-photo" alt=""/>
       <div className = "city-title">
         <h2>{city.name}</h2>
         <p>A description goes here...</p>
