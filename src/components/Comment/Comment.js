ace34a4a706242fb9fd078d0a58e1993e6bf5a5d
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-25 23:11:24 -0800

    comment styling

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,10 +3,14 @@ import './Comment.css'
 
 const Comment = (props) => {
     const {comment} = props
+   
     return(
-      <div className='comment'>
-    <h5>{comment.user.name}</h5>
-    <p className="comment-body">{comment.content}</p>
+    <div className='comment'>
+      <div className="user-detail">
+        <h5>{comment.user.name}</h5>
+        <div className="user-image" style={{backgroundImage:`url(${comment.user.profilePhoto})`}}/>
+      </div>
+      <p className="comment-body">{comment.content}</p>
     </div>
     )
 }
