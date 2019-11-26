a334d80c6f559f465c3e51ec9038e60ab5438ab7
carsonbutterfield <carsonbutterfield98@gmail.com>
2019-11-26 09:05:53 -0800

    formatted time

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -3,8 +3,22 @@ import './Comment.css'
 import { Link, withRouter } from 'react-router-dom';
 const Comment = (props) => {
     const {comment} = props
-    const timeElapsed = (Date.now() - new Date(comment.timestamp).getTime())/1000 / 60
-    console.log(timeElapsed)
+    let timeElapsed = (Date.now() - new Date(comment.timestamp).getTime())/1000 / 60
+    const determineTime = (input) => {
+      let string = "minutes"
+      if(timeElapsed > 60){
+        timeElapsed = timeElapsed / 60
+        string = 'hours'
+      }
+
+      if(timeElapsed > 24) {
+        timeElapsed = timeElapsed / 24
+        string = 'days'
+      }
+      timeElapsed = Math.floor(timeElapsed)
+
+      return `${timeElapsed} ${string} ago`
+    }
     return(
     <div className='comment'>
       <div className="user-detail">
@@ -15,7 +29,7 @@ const Comment = (props) => {
         <div className="user-image" style={{backgroundImage:`url(${comment.user.profilePhoto})`}}/>
       </div>
       <p className="comment-body">{comment.content}</p>
-      <small>{timeElapsed.toFixed(2)} minutes ago</small>
+      <small>{determineTime(timeElapsed)}</small>
 
     </div>
     )
